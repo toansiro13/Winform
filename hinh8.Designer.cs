38c2aca80dc37f82b180e29714e7ecfd0a9c896e
toansiro13 <50589049+toansiro13@users.noreply.github.com>
2019-05-13 22:52:05 +0700

    Create hinh8.Designer.cs

---
+++ hinh8.Designer.cs
@@ -0,0 +1,149 @@
+namespace baigiuaky
+{
+    partial class hinh8
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMau)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox16
+            // 
+            this.pictureBox16.Image = global::baigiuaky.Properties.Resources.hinh8_16;
+            // 
+            // pictureBox15
+            // 
+            this.pictureBox15.Image = global::baigiuaky.Properties.Resources.hinh8_15;
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.Image = global::baigiuaky.Properties.Resources.hinh8_14;
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.Image = global::baigiuaky.Properties.Resources.hinh8_13;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Image = global::baigiuaky.Properties.Resources.hinh8_12;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Image = global::baigiuaky.Properties.Resources.hinh8_11;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = global::baigiuaky.Properties.Resources.hinh8_10;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::baigiuaky.Properties.Resources.hinh8_09;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::baigiuaky.Properties.Resources.hinh8_08;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::baigiuaky.Properties.Resources.hinh8_06;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::baigiuaky.Properties.Resources.hinh8_05;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::baigiuaky.Properties.Resources.hinh8_04;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::baigiuaky.Properties.Resources.hinh8_03;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::baigiuaky.Properties.Resources.hinh8_02;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::baigiuaky.Properties.Resources.hinh8_01;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::baigiuaky.Properties.Resources.hinh8_07;
+            // 
+            // picMau
+            // 
+            this.picMau.Image = global::baigiuaky.Properties.Resources.hinh8_mau;
+            // 
+            // hinh8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "hinh8";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMau)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
