4d5523d9f713c63f4f1cea4ea3292fb87741f825
toansiro13 <50589049+toansiro13@users.noreply.github.com>
2019-05-13 22:45:49 +0700

    Create hinh2.cs

---
+++ hinh2.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baigiuaky
+{
+    public partial class hinh2 : Usctrgame
+    {
+        public hinh2()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox0_Click(object sender, EventArgs e)
+        {
+            nhapchuot(sender, e);
+        }
+    }
+}
